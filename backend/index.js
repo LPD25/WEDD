b504bfd1c9f1e96c512605b9f319c67f8ac50e6e
bertino2 <berolbertindjomo@gmail.com>
2025-06-12 22:22:15 +0200

    modification du cors dans index.js

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,45 @@ const PORT = process.env.PORT || 5000 ;
 const routes = require("./Routes/Routes");
 
 // ✅ Configuration CORS
-const corsOptions = {
- // origin: 'http://localhost:5173', // l'URL du frontend
-  origin: [
-  process.env.FRONTEND_URL, // <-- URL de ton frontend Vercel
-  // "http://localhost:5173"
-],
+// const corsOptions = {
+//  // origin: 'http://localhost:5173', // l'URL du frontend
+//   origin: [
+//   process.env.FRONTEND_URL, // <-- URL de ton frontend Vercel
+//   // "http://localhost:5173"
+// ],
 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // ⚠ inclure DELETE
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // ⚠ inclure DELETE
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// };
+
+// app.use(cors(corsOptions));
+
+
+
+const allowedOrigins = [
+   process.env.FRONTEND_URL, // mets ici l’URL Vercel de ton frontend
+  "http://localhost:5173"
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // autoriser les requêtes sans origin (comme curl ou Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // obligatoire pour gérer les requêtes OPTIONS
+
+
 
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
@@ -45,8 +71,8 @@ app.use("/",routes)
 app.use('/uploads', express.static('uploads'));
 
 
-// app.listen(PORT, function () {
-//     console.log("Serveur à l'écoute sur le port ", PORT);
-// });
+app.listen(PORT, function () {
+    console.log("Serveur à l'écoute sur le port ", PORT);
+});
 
-module.exports = app;
+
