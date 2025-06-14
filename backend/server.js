91575f18152fba25a6f1fd69fb9979baa8d77f1e
bertino2 <berolbertindjomo@gmail.com>
2025-06-14 23:11:46 +0200

    mise en place du lien du frontend

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ const PORT = process.env.PORT || 5000;
 const routes = require("./Routes/Routes");
 
 // Liste des origines autorisées
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: "https://wedd-lake.vercel.app", credentials: true }));
 
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
