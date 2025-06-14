a32a93aabf8a3da0908cb235c1ed6cb06a2d4be1
bertino2 <berolbertindjomo@gmail.com>
2025-06-15 01:04:32 +0200

    retrait du vercel.json

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,28 @@ const PORT = process.env.PORT || 5000;
 const routes = require("./Routes/Routes");
 
 // Liste des origines autorisées
-app.use(cors());
+
+const allowedOrigins = [
+  "https://wedd-sooty.vercel.app", // ton front-end actuel
+  "http://localhost:5173" // pour le dev local
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("CORS bloqué pour l'origine :", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // pour gérer les requêtes préalables OPTIONS
+
 
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
