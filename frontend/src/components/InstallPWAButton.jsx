74a899c4465e2f7186fa0d39f2916ac482e271c0
bertino2 <berolbertindjomo@gmail.com>
2025-06-19 22:11:28 +0200

    ajout du bouton d'installation

--- frontend/src/components/InstallPWAButton.jsx
+++ frontend/src/components/InstallPWAButton.jsx
@@ -44,7 +44,7 @@ function InstallPWAButton() {
   if (!show) return null;
 
   return (
-    <button className="fixed top-0 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-b-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-1 flex items-center justify-between gap-2 w-[80%] md:w-auto">
+    <button onClick={handleInstallClick} className="fixed top-0 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-b-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-1 flex items-center justify-between gap-2 w-[80%] md:w-auto">
       Installer
       <img src={download} alt="download_installation" className="w-5 h-5" />
     </button>
